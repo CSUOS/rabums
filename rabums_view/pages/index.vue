69188b9ff87796cb6f618fa40d7eef99fffb861e
iWan <iwanhae@gmail.com>
2020-08-05 23:58:21 +0900

    프론트 개발 시작.

---
+++ rabums_view/pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container">
+    <div>
+      <img src="/logo.png" style="width: 80%; max-width: 1024px;" />
+      <div class="links">
+        <nuxt-link to="/edit" style="margin: 0 10px;">
+          <el-button class="main_button" type="primary" plain>
+            회원정보수정
+          </el-button>
+        </nuxt-link>
+        <nuxt-link to="/register" style="margin: 0 10px;">
+          <el-button class="main_button" type="success" plain>
+            회원가입
+          </el-button>
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+a {
+  color: black;
+  text-decoration: none;
+}
+.main_button {
+  font-size: large;
+  padding: 20px;
+}
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ rabums_view/pages/register.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="background">
+    <div class="register_container">
+      <div class="line">
+        <h2>
+          RABUMS
+        </h2>
+      </div>
+      <div class="line">
+        <el-radio v-model="type" label="1" border>학생</el-radio>
+        <el-radio v-model="type" label="2" border>교수/강사</el-radio>
+      </div>
+      <div class="line">
+        <div class="title">
+          <p style="margin: 10px auto;">이름 :</p>
+        </div>
+        <el-input placeholder="Please input" v-model="input"></el-input>
+      </div>
+      <div class="line">
+        <div class="title">
+          <p style="margin: 10px auto;">학번 :</p>
+        </div>
+        <el-input placeholder="Please input" v-model="input"></el-input>
+      </div>
+      <div class="line">
+        <div class="title">
+          <p style="margin: 10px auto;">아이디 :</p>
+        </div>
+        <el-input placeholder="Please input" v-model="input"></el-input>
+      </div>
+      <div class="line">
+        <div class="title">
+          <p style="margin: 10px auto;">비밀번호 :</p>
+        </div>
+        <el-input placeholder="Please input" v-model="input"></el-input>
+      </div>
+      <div class="line">
+        <div class="title">
+          <p style="margin: 10px auto;">비밀번호 확인 :</p>
+        </div>
+        <el-input placeholder="Please input" v-model="input"></el-input>
+      </div>
+      <div class="line">
+        <div class="title">
+          <p style="margin: 10px auto;">이메일주소 :</p>
+        </div>
+        <el-input placeholder="Please input" v-model="input"></el-input>
+      </div>
+      <div class="line">
+        <nuxt-link to="/" style="margin: 0 10px;">
+          <el-button type="info" plain>
+            취소
+          </el-button>
+        </nuxt-link>
+        <el-button style="margin: 0 10px;" type="primary" plain>
+          제출
+        </el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      type: 0,
+    }
+  },
+}
+</script>
+
+<style>
+.title {
+  text-align: initial;
+  font-size: 16px !important;
+  min-width: 160px;
+}
+.line {
+  margin: auto;
+  display: flex;
+  margin: 10px;
+}
+.background {
+  display: flex;
+  height: 100vh;
+  background: whitesmoke;
+}
+.register_container {
+  margin: auto;
+  min-height: 640px;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background: white;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+  width: 80%;
+  max-width: 1024px;
+  min-width: 360px;
+}
+</style>
